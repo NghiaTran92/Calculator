1d6d0c261b281d67b3d75f9ce4a61ea0ca8e17a4
Nghia Tran <thanhnghiaglhn92@gmail.com>
2015-09-09 06:41:09 +0700

    custom view

--- app/src/main/java/com/nghiatt/calculator/custom/Button.java
+++ app/src/main/java/com/nghiatt/calculator/custom/Button.java
@@ -1,22 +1,41 @@
 package com.nghiatt.calculator.custom;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
 import android.util.AttributeSet;
+
+import com.nghiatt.calculator.R;
 
 /**
  * Created by FRAMGIA\tran.thanh.nghia on 08/09/2015.
  */
 public class Button extends android.widget.Button {
     public Button(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public Button(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public Button(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        if (!isInEditMode()) {
+            TypedArray a = context.obtainStyledAttributes(attrs,
+                    R.styleable.TextView);
+            String font = a.getString(R.styleable.TextView_font);
+            if (font != null) {
+                if (!font.contains("."))
+                    font += ".ttf"; // Add default font's extension.
+                Typeface typeface = Typeface.createFromAsset(context.getAssets(), font);
+                if (typeface != null) {
+                    setTypeface(typeface);
+                }
+                //Log.i("Viki TextView", "set font: " + font);
+            }
+            a.recycle();
+        }
     }
 
 
--- app/src/main/java/com/nghiatt/calculator/custom/TextView.java
+++ app/src/main/java/com/nghiatt/calculator/custom/TextView.java
@@ -1,21 +1,40 @@
 package com.nghiatt.calculator.custom;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
 import android.util.AttributeSet;
+
+import com.nghiatt.calculator.R;
 
 /**
  * Created by FRAMGIA\tran.thanh.nghia on 08/09/2015.
  */
 public class TextView extends android.widget.TextView {
     public TextView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public TextView(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public TextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        if (!isInEditMode()) {
+            TypedArray a = context.obtainStyledAttributes(attrs,
+                    R.styleable.TextView);
+            String font = a.getString(R.styleable.TextView_font);
+            if (font != null) {
+                if (!font.contains("."))
+                    font += ".ttf"; // Add default font's extension.
+                Typeface typeface = Typeface.createFromAsset(context.getAssets(), font);
+                if (typeface != null) {
+                    setTypeface(typeface);
+                }
+                //Log.i("Viki TextView", "set font: " + font);
+            }
+            a.recycle();
+        }
     }
 }
